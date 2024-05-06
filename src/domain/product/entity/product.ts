c8f4c1edfe38f75703ab8a6d6e999bdfe1193547
Felipe <fgoncalves2008@gmail.com>
2024-05-06 09:56:50 -0300

    Adicionando validação(notification errors) sem YUP

--- src/domain/product/entity/product.ts
+++ src/domain/product/entity/product.ts
@@ -41,6 +41,22 @@ export default class Product extends Entity implements ProductInterface {
   }
 
   validate() {
-    ProductValidatorFactory.create().validate(this);
+    if (this.id.length === 0)
+      this.notification.addError({
+        context: 'product',
+        message: "Id is required"
+      });
+    if (this.name.length === 0)
+      this.notification.addError({
+        context: 'product',
+        message: "Name is required"
+      });
+    
+    if (this.price < 0)
+        this.notification.addError({
+          context: 'product',
+          message: "Price must be greater than zero"
+        });
+    // ProductValidatorFactory.create().validate(this);
   }
 }
